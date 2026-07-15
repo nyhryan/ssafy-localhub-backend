56f9516d3f4ffa2d3b5cced14749f6efe021370e
YunHyeok Nam <nyhryan99102@gmail.com>
2026-07-15 16:23:54 +0900

    chore: format source codes

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from .database import Base, engine
 from . import models
 from .api.v1 import greeting, category, posts
 
-
-
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
@@ -20,7 +18,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(greeting.router, prefix="/api/v1")
 app.include_router(posts.router, prefix="/api/v1")
